ecfb5e2ce622ac2afd6e9480e6cec300f8e46ef2
lessfish <lessfish@outlook.com>
2024-06-05 19:13:12 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,21 @@
+# README
+
+## install
+
+```shell
+cd open_repo_from_cmd_to_browser
+npm link
+```
+
+## usage
+
+```shell
+# 进入项目根目录
+cd ...
+
+# 在浏览器中打开项目
+$ go
+
+# 在浏览器中打开项目的具体分支
+$ go b # alias `go branch`
+```
---
+++ cli.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+const shelljs = require("shelljs");
+const colors = require("colors");
+const open = require("open");
+
+const args = process.argv;
+const goToBranch = args[2] === "branch" || args[2] === "b";
+
+shelljs.exec(
+  "git remote -v",
+  { silent: true },
+  function (code, stdout, stderr) {
+    if (code !== 0) {
+      console.log(colors.red(stderr.trim()));
+      return;
+    }
+
+    if (stdout.trim() === "") {
+      console.log(colors.red("当前目录没有绑定 remote git 仓库"));
+      return;
+    }
+
+    // str e.g.
+    // origin	git@github.com:demo-space/third-party-cookie-check.git (fetch)
+    // origin	git@code.byted.org:cloud-fe/onesite.git (fetch)
+    // origin	git@github.com:lessfish/jsj.git (fetch)
+    const str = stdout.split("\n")[0];
+
+    const p = /git@(.*):(.*)\/(.*)\.git/;
+    const res = p.exec(str);
+    const url = `https://${res[1]}/${res[2]}/${res[3]}`;
+
+    if (!goToBranch) {
+      return open(url);
+    }
+
+    // go to branch
+    shelljs.exec(
+      "git symbolic-ref --short HEAD",
+      { silent: true },
+      function (code, stdout, stderr) {
+        if (code !== 0) {
+          console.log(colors.red(stderr));
+          return;
+        }
+
+        open(`${url}/tree/${stdout.trim()}`);
+      }
+    );
+  }
+);
